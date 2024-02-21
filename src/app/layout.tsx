829bcf329e438dc06798427305a70c545d7cc0a5
Florian Bourdenet <florian.bourdenet@lucielabs.com>
2024-02-21 15:47:09 +0100

    added more icons and modified manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next';
 import { IBM_Plex_Mono } from 'next/font/google'
 
 import "./globals.css";
@@ -7,6 +8,11 @@ const IBM_PLEX_MONO = IBM_Plex_Mono({
     weight: ['400', '500', '600', '700'],
     subsets: ['latin'],
 })
+
+export const metadata: Metadata = {
+    title: "Florian Bourdenet",
+    description: "hey ! my name is florian, i'm 23 years old, and i'm a french full-stack developer based in nice, france.",
+}
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,11 +7,21 @@ export default function manifest(): MetadataRoute.Manifest {
         description: "hey ! my name is florian, i'm 23 years old, and i'm a french full-stack developer based in nice, france.",
         icons: [
             {
+                src: "icon_256x256.jpg",
+                sizes: "256x256",
+            },
+            {
+                src: "icon_128x128.jpg",
+                sizes: "128x128",
+            },
+            {
                 src: "favicon.ico",
                 sizes: "48x48",
                 type: "image/x-icon"
             }
         ],
         start_url: "/",
+        display: "standalone",
+        orientation: "landscape"
     }
 }
